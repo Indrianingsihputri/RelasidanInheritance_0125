8ef1e9c6fa87de26e6f52969ae15b192914c3197
Indrianingsihputri <indrianingsihputri30@gmail.com>
2025-05-27 20:10:30 +0700

    membuat ifndef IBU_H, define IBU_H, dan membuat class ibu

---
+++ Agregasi/ibu.h
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#ifndef IBU_H
+#define IBU_H
+
+class ibu {
+    public:
+        string nama;
+        vector<anak*> daftar_anak;
+
+        ibu(string pNama) :nama(pNama) {
+            cout << "Ibu \"" << nama << "\" ada\n";
+        }
+
+        ~ibu() {
+            cout << "Ibu \"" << nama << "\" tidak ada\n";
+        }
+
+        void tambahAnak(anak*);
+        void cetakAnak();
+};
+
