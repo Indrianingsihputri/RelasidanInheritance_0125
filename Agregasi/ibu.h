81f4086ec6003368c55460451ae33d22399efd20
Indrianingsihputri <indrianingsihputri30@gmail.com>
2025-05-27 20:12:55 +0700

    menambahkan perinta void ibu tambah anak, dan cetak anak

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -22,3 +22,21 @@ class ibu {
         void cetakAnak();
 };
 
+vioid ibu::tambahAnak(anak* pAnak) {
+    daftar_anak.push_back(pAnak);
+}
+
+void ibu::cetakAnak() {
+    cout << "daftar anak dari ibu \"" << this->nama << "\":\n";
+    /*for (auto& a : daftar_anak) {
+        cout << a->nama << "\n"
+    }*/
+
+    for (int i = 0; i < daftar_anak.size(); i++) {
+        cout << daftar_anak[i]->nama << endl;
+    }
+
+    cout << endl;
+}
+
+#endif // IBU_H
